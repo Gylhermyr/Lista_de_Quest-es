b5068a958f7d904a20f7f449478944fc0603a8c5
Gylhermyr <jguimouraaraujo2009@gmail.com>
2025-05-29 11:02:59 -0300

    Logica_Computacional_Programacao.c

---
+++ Lista de Exercícios - Lógica Computacional/Logica_Computacional_Programacao.c
@@ -0,0 +1,19 @@
+
+//Questão 34
+
+/*#include <stdio.h>
+
+int main() {
+    int telefone = 0, idade = 0;
+    char nome[100], endereco[100];
+    printf("Fale seu nome(Não use espaço) \n");
+    scanf("%s", &nome);
+    printf("Fale sua idade \n");
+    scanf("%d", &idade);
+    printf("Fale seu endereço(Não use espaço) \n");
+    scanf("%s", &endereco);
+    printf("Fale seu telefone(Não use espaço) \n");
+    scanf("%d", &telefone);
+    printf ("Meu nome é %s, tenho %d anos e moro em %s. Meu número de telefone é %d.", nome, idade, endereco, telefone);
+    return 0;
+}*/
