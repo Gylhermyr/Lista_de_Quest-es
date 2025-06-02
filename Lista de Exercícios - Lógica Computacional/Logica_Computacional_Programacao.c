4659169a951e142b4f1eee0508abfde71c8f3fa5
Gylhermyr <jguimouraaraujo2009@gmail.com>
2025-06-02 09:42:25 -0300

    Logica_Computacional_Programacao.c

--- Lista de Exercícios - Lógica Computacional/Logica_Computacional_Programacao.c
+++ Lista de Exercícios - Lógica Computacional/Logica_Computacional_Programacao.c
@@ -1,3 +1,47 @@
+//Questão 28
+
+/*#include <stdio.h>
+
+int main() {
+    float valor_gasto;
+    int cod = 0;
+    int mes = 0;
+    printf("Qual o valor gasto por você? \n");  
+    scanf("%f", &valor_gasto);
+    printf("Digite o número que deseja realizar a forma de pagamento\n 1: A vista com desconto de 10%%?\n 2: Parcelado em 2 vezes sem juros?\n 3: Parcelado em 3 até 10 vezes com 3%% de juros?(apenas para compras acima de 100). \n");
+    scanf("%d", &cod);
+    if(cod == 3 && valor_gasto < 100){
+        printf("Não permitimos que valores menores que 100 sejam parcelado em 3 vezes ou acima.");
+    }
+    if(cod<1 || cod>3){
+        printf("Não existe essa opção de pagamento.");
+    }
+    if(cod == 1){
+        float total_gasto = (valor_gasto * 0.90);
+        printf("Sua compra deu R$%3.2f. \n", total_gasto);
+    }
+    if(cod == 2){
+        float total_gasto = valor_gasto / 2;
+        printf("Sua compra deu R$%3.2f durante 2 meses. \n", total_gasto);
+    }
+    if(cod == 3 && valor_gasto >= 100){
+        printf("Quantos meses deseja parcelar? \n");
+        scanf("%d", &mes);
+        float valor_indice_juros = 0.03;
+        float total_gasto = ((valor_gasto * valor_indice_juros) * mes) +
+        valor_gasto;
+        float valor_mes = total_gasto / mes;
+        printf("Sua compra deu R$%3.2f, ficando %3.2f durante %d meses. \n",
+            total_gasto, valor_mes, mes);
+    }
+    if(mes>=3 || mes<=10){
+        
+    }
+    else{
+        printf("Não é possivel parcelar por essa quantidade de meses. \n");
+    }
+    return 0;
+}*/
 
 //Questão 34
 
