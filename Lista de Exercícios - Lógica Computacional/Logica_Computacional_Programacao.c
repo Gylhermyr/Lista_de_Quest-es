a5fdb6e4d269266b6325ae72a56e31a1a6697845
Gylhermyr <jguimouraaraujo2009@gmail.com>
2025-06-04 07:25:09 -0300

    Logica_Computacional_Programacao.c

--- Lista de Exercícios - Lógica Computacional/Logica_Computacional_Programacao.c
+++ Lista de Exercícios - Lógica Computacional/Logica_Computacional_Programacao.c
@@ -5,7 +5,6 @@
 int main() {
     float valor_gasto;
     int cod = 0;
-    int mes = 0;
     printf("Qual o valor gasto por você? \n");  
     scanf("%f", &valor_gasto);
     printf("Digite o número que deseja realizar a forma de pagamento\n 1: A vista com desconto de 10%%?\n 2: Parcelado em 2 vezes sem juros?\n 3: Parcelado em 3 até 10 vezes com 3%% de juros?(apenas para compras acima de 100). \n");
@@ -25,20 +24,20 @@ int main() {
         printf("Sua compra deu R$%3.2f durante 2 meses. \n", total_gasto);
     }
     if(cod == 3 && valor_gasto >= 100){
+        int mes = 0;
         printf("Quantos meses deseja parcelar? \n");
         scanf("%d", &mes);
-        float valor_indice_juros = 0.03;
-        float total_gasto = ((valor_gasto * valor_indice_juros) * mes) +
-        valor_gasto;
-        float valor_mes = total_gasto / mes;
-        printf("Sua compra deu R$%3.2f, ficando %3.2f durante %d meses. \n",
-            total_gasto, valor_mes, mes);
-    }
-    if(mes>=3 || mes<=10){
-        
-    }
-    else{
-        printf("Não é possivel parcelar por essa quantidade de meses. \n");
+        if(mes<=3 || mes>=10){
+            printf("Não é possivel parcelar por essa quantidade de meses. \n");
+        }
+        else{
+            float valor_indice_juros = 0.03;
+            float total_gasto = ((valor_gasto * valor_indice_juros) * mes) +
+            valor_gasto;
+            float valor_mes = total_gasto / mes;
+            printf("Sua compra deu R$%3.2f, ficando %3.2f durante %d meses. \n",
+                total_gasto, valor_mes, mes);
+        }
     }
     return 0;
 }*/
